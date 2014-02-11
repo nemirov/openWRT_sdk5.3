26eefaf5adaf82dc8281ebf9ae7ea82e45b33dc3
nemirov <pmnemirov@gmail.com>
2014-02-11 11:00:20 +0400

    Add socket server

--- rs_232.py
+++ rs_232.py
@@ -17,6 +17,12 @@ import time
 file_rs232 = '/dev/ttyUSB0'
 params_sdk = {'hw': 0, 'sw': 0, 'self_temp': 0, 'relay': 0, 'optical_relay': (), 'dry_contact': ()}
 
+HOST = '127.0.0.1'  
+PORT = 10500 
+MAX_CLIENTS = 100 
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 fd = serial.Serial(
                    port = file_rs232,
                    baudrate = 57600,
@@ -24,15 +30,17 @@ fd = serial.Serial(
 try: 
 
     fd.open()
-
+    s.bind((HOST, PORT))
+    s.listen(MAX_CLIENTS)
+    
 except Exception, e:
 
-    print "Error open serial port: " + str(e)
+    print "Error init port: " + str(e)
 
     exit()
     
  
-        
+#------------------------RS232-------------------------------------------------------------------        
         
 def parse_status(value):
     params_sdk['hw'] = int(value[6:8], 16)
@@ -74,10 +82,8 @@ def run():
             word_3 = 'TSC1C105000000000005\r\n' 
         
             while 1:
-                print "request"
                 answer_status = get_status(word_1)
                 parse_status(answer_status)
-            
                 answer_word2 = get_status(get_status(word_2))
                 answer_word3 = get_status(get_status(word_3))  
                    
@@ -90,10 +96,43 @@ thread_sdk = threading.Thread(target = run, args = ())
 thread_sdk.start()
 
 
-if thread_sdk.isAlive():
-    time.sleep(3)
-    for key, value in params_sdk.items():
-        print key + " : " + str(value)
+
+        
+
+#------------------------socket server-------------------------------------------------------------------       
 
 
+def clientthread(conn):
+    while True:   
+        data = conn.recv(1024)
+        #reply = 'OK...' + data
+        if not data:
+            break
+        
+        if str(data) == 'get_hw':
+            answer = str(params_sdk['hw'])
+        elif str(data) == 'get_sw':
+            answer = str(params_sdk['sw'])
+        elif str(data) == 'get_self_temp':
+            answer = str(params_sdk['self_temp'])
+        elif str(data) == 'get_relay':
+            answer = str(params_sdk['relay'])
+        elif str(data) == 'get_optical_relay':
+            answer = str(params_sdk['optical_relay'])
+        elif str(data) == 'get_dry_contact':
+            answer = str(params_sdk['dry_contact'])
+            
+        else:
+            answer = str(('get_hw', 'get_sw', 'get_self_temp', 'get_relay', 'get_optical_relay', 'get_dry_contact' ))    
+            
+        conn.send(answer)
+    conn.close()
+ 
+
+while 1:
+    conn, addr = s.accept()
+    thread_socet = threading.Thread(target = clientthread ,args = (conn,))
+    thread_socet.start()
+ 
+s.close()
 
